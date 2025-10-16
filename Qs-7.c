06267cdf5ef63fce17377dcefdf439b1efbdd675
Syed Muhammad Ali <k250963@nu.edu.pk>
2025-10-16 21:33:37 +0500

    Update Qs-7.c

--- Qs-7.c
+++ Qs-7.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
-
-int main(){
-	
-	int array[10];
-	int duplicate[100];
-	int y = 0;
-	for(y; y< 100; y++){	
-		duplicate[y] = 0;
-	}
-	int i =0;
-	for(i; i<10 ; i++){	
-		printf("Enter The Number: ");
-		scanf("%d", &array[i]);
-	}
-	int j = 0;
-	int q;
-	for(j; j < 10; j++){	
-		q = array[j] - 1;
-		if(duplicate[q] == 0){
-			duplicate[q] = 1;
-		}else{		
-			array[j] = -1;
-		}
-	}
-	int x = 0;
-	for(x; x<10; x++){
-		printf("%d", array[x]);
-		printf(" ");
-	}
+int main() {
+    int array[10]; 
+    int duplicate[100];
+    int x,i,j,q,p;
+    for (x = 0; x < 100; x++) {
+        duplicate[x] = 0;
+    }
+    printf("Enter 10 Positive Integers(1 to 100): \n");
+    for (i=0; i < 10; i++) {
+        printf("Enter Number %d: ", i+1);
+        scanf("%d", &array[i]);
+    }
+    for (j = 0; j < 10; j++) {
+        q = array[j] - 1;
+        if (q >= 0 && q < 100) {
+            if (duplicate[q] == 0) {
+                duplicate[q] = 1;
+            } else {
+                array[j] = - 1;
+            }
+        }
+    }
+    for (p=0; p < 10; p++) {
+        printf("%d", array[p]);
+        if (p < 9) {
+            printf(" ");
+        }
+    }
+    printf("\n");
     return 0;
 }
